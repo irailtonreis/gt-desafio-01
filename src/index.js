67be8c4b6fab30ea802aeb5beaf8c57f1262ac5d
irailtonreis <irailton@gmail.com>
2020-02-24 11:09:50 -0300

    Adicionando listagem, e atualização

--- src/index.js
+++ src/index.js
@@ -7,9 +7,28 @@ server.use(express.json())
 server.use(routes);
 
 const projects = [];
+
 server.post("/projects", (req, res) => {
   
+  projects.push(req.body);
+
   return res.json(req.body);
+})
+
+server.get('/projects', (req, res)=>{
+  return res.json(projects);
+})
+
+
+server.put('/projects/:id', (req, res)=>{
+  const result = projects.filter(project => {
+    if(project.id == req.params.id){
+      project.title = req.body.title;
+    }
+    return project;
+  });
+  
+  return res.json(result);
 })
 
 server.listen(3333);
